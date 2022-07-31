7ef259f794fddb611612f8ce1c141d10396acf91
Udbhav Ram <udbhavram41@gmail.com>
2022-07-31 13:48:15 -0400

    many models and ai
    chatbot ai should be working, @MehtabW27 @mathunada07 and @manan, we need to create more training data

--- humm_backend/chat/chatbot.py
+++ humm_backend/chat/chatbot.py
@@ -1,9 +1,20 @@
 import nltk
 # nltk.download()
+from chatterbot import ChatBot  # import the chatbot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+print(os.getcwd())
+bot = ChatBot('Bot')
+trainer = ChatterBotCorpusTrainer(bot)
+
+corpus_path = r'C:\Users\sathy\OneDrive\Documents\GitHub\humm\humm_backend\chat\corpus_english/'
+
+for file in os.listdir(corpus_path):
+    trainer.train(corpus_path + file)
+
 from nltk.sentiment import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
-def analyze_response(input_text):
-
+def sentiment_analysis(input_text):
     response = sia.polarity_scores(input_text)
     if response['compound'] > 0.5:
         return "I am happy to hear that!"
@@ -11,28 +22,40 @@ def analyze_response(input_text):
         return "I am sorry to hear that!"
     else:
         return "I am neutral on that."
+def if_else(input_text):
+    if input_text == "how are you":
+        return "I am fine, thank you!"
+    elif input_text == "what is your name":
+        return "My name is Humm, and I am a chatbot."
+    elif input_text == "what is your favorite color":
+        return "My favorite color is blue."
+    elif input_text == "what is your favorite food":
+        return "My favorite food is pizza."
+    elif input_text == "what is your favorite movie":
+        return "My favorite movie is Star Wars."
+    elif input_text == "what is your favorite song":
+        return "My favorite song is The Sign by Ace of Base."
+    elif input_text == "what is your favorite sport":
+        return "My favorite sport is basketball."
+    elif input_text == "what is your favorite animal":
+        return "My favorite animal is a cat."
+    elif input_text == "help" or "HELP":
+        return help_text()
+    else:
+        return "I do not understand, please type HELP for a list of commands."
 
-    # if input_text == "how are you":
-    #     return "I am fine, thank you!"
-    # elif input_text == "what is your name":
-    #     return "My name is Humm, and I am a chatbot."
-    # elif input_text == "what is your favorite color":
-    #     return "My favorite color is blue."
-    # elif input_text == "what is your favorite food":
-    #     return "My favorite food is pizza."
-    # elif input_text == "what is your favorite movie":
-    #     return "My favorite movie is Star Wars."
-    # elif input_text == "what is your favorite song":
-    #     return "My favorite song is The Sign by Ace of Base."
-    # elif input_text == "what is your favorite sport":
-    #     return "My favorite sport is basketball."
-    # elif input_text == "what is your favorite animal":
-    #     return "My favorite animal is a cat."
-    # elif input_text == "what is your favorite book":
-    #     return "My favorite book is The Hobbit by J.R.R. Tolkien."
-    # elif input_text == "what is your favorite game":
-    #     return "My favorite game is League of Legends."
-    # elif input_text == "what is your favorite movie":
-    #     return "My favorite movie is Star Wars."
-    # else:
-    #     return "I do not understand."
+def help_text():
+    return "I can answer questions about your life. Here are some examples: \n\n" + \
+           "How are you? \n\n" + \
+           "What is your name? \n\n" + \
+           "What is your favorite color? \n\n" + \
+           "What is your favorite food? \n\n" + \
+           "What is your favorite movie? \n\n" + \
+           "What is your favorite song? \n\n" + \
+           "What is your favorite sport? \n\n" + \
+           "What is your favorite animal? \n\n" + \
+           "If you want to know more about me, type HELP."
+
+def ai_chat(input_text):
+    reply = bot.get_response(input_text)
+    return reply
