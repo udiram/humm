022d15c0aef60efdfee06ae0a5b9c203c7b51d68
Mathurshan Nadarajar <mathunada07@gmail.com>
2022-08-07 13:00:04 -0400

    Minor Updates to UI
    Added navbar, reformatted time of message sent, added alignment based on sender/receiver of message

--- humm-frontend/src/App.tsx
+++ humm-frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./images/logo/2x/humm_logo@2x.png";
 import "./App.css";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./navbar";
 
 interface AppProps {}
 
@@ -30,10 +32,14 @@ class App extends Component<AppProps, AppState> {
 
   sendMessage(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    const dateFormatter = new Intl.DateTimeFormat("en-CA", {
+      hour: "numeric",
+      minute: "numeric",
+    });
 
     let message: message = {
       text_contents: event.currentTarget.message.value,
-      text_date: new Date().toISOString(),
+      text_date: dateFormatter.format(new Date()),
       text_author: "me",
     };
 
@@ -46,9 +52,9 @@ class App extends Component<AppProps, AppState> {
         let response_text = res.data;
         let response: message = {
           text_contents: response_text,
-          text_date: new Date().toISOString(),
-          text_author: 'bot',
-        }
+          text_date: dateFormatter.format(new Date()),
+          text_author: "bot",
+        };
         this.state.messages.push(response);
         this.setState({ messages: this.state.messages });
       })
@@ -58,19 +64,38 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <main className="container">
-        <h1>Humm Chatbot</h1>
-        <div className="messages">
+        <Navbar />
+        <div className="row">
+          <div className="col-3"></div>
+          <div className="col-6 justify-content-center">
+            <img className="my-2 img-fluid" src={logo}></img>
+          </div>
+          <div className="col-3"></div>
+        </div>
+        <div className="messages col-6 mx-auto">
           {this.state.messages.map((message) => {
             return (
-              <div key={message.text_date}>
-                {message.text_author + ": " + message.text_contents}
+              <div
+                key={message.text_date}
+                className={
+                  message.text_author == "bot"
+                    ? "m-2 text-start"
+                    : "m-2 text-end"
+                }
+              >
+                <div className="">{message.text_date}</div>
+                <div className="">{message.text_contents}</div>
               </div>
             );
           })}
         </div>
         <form onSubmit={(event) => this.sendMessage(event)}>
-          <input type="text" name="message"></input>
-          <input type="submit"></input>
+          <div className="col-3 mx-auto justify-content-center">
+            <input type="text" name="message"></input>
+            <button type="submit">
+              <i className="fa fa-paper-plane humm-orange"></i>
+            </button>
+          </div>
         </form>
       </main>
     );
