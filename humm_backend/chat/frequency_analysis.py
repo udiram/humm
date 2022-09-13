9529d0b80b132a29c4cf8127ccce109b0ab93d24
Udbhav Ram <udbhavram41@gmail.com>
2022-09-12 22:54:20 -0400

    cleanup of freq_analysis

--- humm_backend/chat/frequency_analysis.py
+++ humm_backend/chat/frequency_analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from nltk import tokenize
-from operator import itemgetter
 import os
 
 def get_highest_frequency(dic_element):
@@ -10,21 +9,14 @@ def get_highest_frequency(dic_element):
 def main_analysis(text):
     path = os.path.join(os.path.dirname(__file__), 'data/' + 'mental_health_keywords.csv')
     df = pd.read_csv(path)
-    df.head()
-    print("hello")
 
     df_monogram = df[df.n <= 1]
-    df_monogram.head()
-    df_monogram.info()
 
     total_words = tokenize.word_tokenize(text)
-    print(total_words)
-
     total_words_filter = list(filter(lambda x: x != ','
                                                and x != '.'
                                                and x != '!'
                                                and x != '?', total_words))
-    print(total_words_filter)
 
     tf = {}
     monograms = dict(zip(df_monogram['ngram'], df_monogram['disorder']))
@@ -37,11 +29,7 @@ def main_analysis(text):
             else:
                 tf[disorder] = 1
 
-    print(tf)
-
     diagnosis = get_highest_frequency(tf)
-    print(diagnosis)
-
     disorder_list = []
 
     for disorders in diagnosis:
@@ -49,8 +37,6 @@ def main_analysis(text):
         for disorder in separated_disorders:
             if disorder not in disorder_list:
                 disorder_list.append(disorder)
-    print(diagnosis)
-    print(disorder_list)
 
     if len(disorder_list) == 0:
         disorder_list.append('general')
