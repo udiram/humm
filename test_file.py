73dd926b4e6e1be2d055481029f708ebe661e6d6
Udbhav Ram <udbhavram41@gmail.com>
2022-09-18 14:49:13 -0400

    circleci

--- test_file.py
+++ test_file.py
@@ -1,29 +1,17 @@
 import unittest
-from django.test import TestCase
+
+from my_sum import sum
+
 from humm_backend.chat import frequency_analysis
 
-class YourTestClass(TestCase):
-    @classmethod
-    # def setUpTestData(cls):
-    #     print("setUpTestData: Run once to set up non-modified data for all class methods.")
-    #     pass
-    #
-    # def setUp(self):
-    #     print("setUp: Run once for every test method to setup clean data.")
-    #     pass
-    #
-    # def test_false_is_false(self):
-    #     print("Method: test_false_is_false.")
-    #     self.assertFalse(False)
-    #
-    # # def test_false_is_true(self):
-    # #     print("Method: test_false_is_true.")
-    # #     self.assertTrue(False)
-    #
-    # def test_one_plus_one_equals_two(self):
-    #     print("Method: test_one_plus_one_equals_two.")
-    #     self.assertEqual(1 + 1, 2)
-
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1,2,3]
+        result = sum(data)
+        self.assertEqual(result, 6)
     def test_freq_analysis(self):
         print("Method: freq_analysis.")
         self.assertEqual(frequency_analysis.main_analysis('hello'), ['general'])
@@ -86,4 +74,6 @@ class YourTestClass(TestCase):
         deal with it please let me know, thank you and take care. feel free to dm if you want to 
         rant, discuss, or give advice on anything regarding me, or you. (:"""),
                          ['ptsd'])
-# Create your tests here.
+
+if __name__ == '__main__':
+    unittest.main()
