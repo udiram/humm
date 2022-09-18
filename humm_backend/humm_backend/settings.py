5a93372f1057f61329ae040611ace7cfdce275a9
mathunada07 <mathunada07@gmail.com>
2022-09-18 03:16:56 -0400

    Update settings.py

--- humm_backend/humm_backend/settings.py
+++ humm_backend/humm_backend/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/')]
+
+if not DEBUG:
+        STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
